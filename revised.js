74aa2fb3a9c56bcc545d64435cd78bbd9ac00eb4
Harold <haroldquekhere@gmail.com>
2023-02-17 21:46:56 +0800

    fix lives

--- revised.js
+++ revised.js
@@ -8,47 +8,43 @@ let maxLevel;
 let selectDifficultyLevel;
 let numberOfLives;
 
-
 if(!gameStarted){
 	$('.container').css('pointer-events', 'none');
 }
 
 function maskDifficultyLevel(){
-  console.log(`selectDifficultyLevel: ${selectDifficultyLevel}`);
 	$('.dropdown-content a').not(`:contains(${selectDifficultyLevel})`).css({
 		'pointer-events': 'none',
 		'opacity': '0.5'
 	});
-	
 }
 
 function difficultyLevel(selectDifficultyLevel){
 	switch (selectDifficultyLevel) {
 		case 'Easy':
-		maxLevel = 20;
-		numberOfLives = 0;   
-		break;
-		
+			maxLevel = 20;
+			numberOfLives = 0;   
+			break;
+
 		case 'Normal':
-		maxLevel = 2;
-		numberOfLives = 3;  
-		break;
-		
 		case 'Difficult':
-		maxLevel = 20;  
-		numberOfLives = 3;  
-		break;
-		
+			maxLevel = 2;
+			numberOfLives = 3;			
+			break;
+
 		default:
-		$('#enter').css('cursor', 'none');  
-		break;
+			$('#enter').css('cursor', 'none');  
+			break;
 	}
-	return maxLevel;
+	return{maxLevel, numberOfLives} ;
 }
+
+let difficulty = difficultyLevel(selectDifficultyLevel);
+maxLevel = difficulty.maxLevel;
+numberOfLives = difficulty.numberOfLives;
 
 function getDifficuiltyLevel(difficultyLevel) {
 	$('.dropdown-content a').click(function() {
-		
 		const clickDropDownContent = $(this);
 		if (selectDifficultyLevel !== '' && clickDropDownContent.text() !== selectDifficultyLevel) {
 			$('.dropdown-content a').removeClass('pressed');
@@ -87,22 +83,19 @@ $('.btn').click(function(){
 function generateColorCombination(getGameLevel){
     userClickPattern = [];
     //gameLevel++;
-    maxLevel = difficultyLevel(selectDifficultyLevel);
+   // maxLevel = difficultyLevel(selectDifficultyLevel);
     $('#level-title').text(`Level ${gameLevel}/Level ${maxLevel}`);
     $('.container').css('pointer-events', 'auto');
     gamePattern = [];
-
+	
     for(let i = 0; i < gameLevel; i++){
 		
 		if (gameLevel > maxLevel) {
 			checkWin();
 			break;
 		}
-		
         const colorIndex = Math.floor(Math.random() * buttonColors.length);
-        console.log(`colorIndex: ${colorIndex}`);
         const color = buttonColors[colorIndex];
-        console.log('Color: ' + color);
         gamePattern.push(color);
         gamePattern.forEach((color, index) => {
 			setTimeout(() => {
@@ -113,7 +106,6 @@ function generateColorCombination(getGameLevel){
         console.log("gamePattern: " + gamePattern);
 	}
 	getGameLevel(gameLevel); //callback function
-	gameWon = false;
 }
 
 function getGameLevel(gameLevel){
@@ -128,7 +120,6 @@ function checkWin(){
 		$('.container').css('pointer-events', 'none');
 		playOtherSounds('winner');
 	}
-
 }
 
 function buttonPressed(btnColor){
@@ -147,7 +138,6 @@ $('body').keypress(function(e){
 		setTimeout(() => {
 			generateColorCombination(getGameLevel);
 		}, 1000);     
-		
 	}
 });
 
@@ -167,13 +157,11 @@ function checkAns() {
 		return;
 	}
 	let lastColor = userClickPattern.length - 1;
-	console.log(`checking gamePattern[lastColor]: ${gamePattern[lastColor]}`);
 	if (userClickPattern[lastColor] !== gamePattern[lastColor] && numberOfLives > 0) { //comparing values in an array
 		console.log('wrong');
 		console.log("userClickPattern[lastIndex]", userClickPattern[lastColor]);
 		console.log("gamePattern[lastIndex]", gamePattern[lastColor]);
-		numberOfLives-=1;
-		console.log(`numberOfLives, ${numberOfLives}`);
+		console.log(selectDifficultyLevel);
 		playOtherSounds('wrong');
 		setTimeout(() => {
             $('body').removeClass('game-over');
@@ -182,6 +170,9 @@ function checkAns() {
 		setTimeout(() => {
             generateColorCombination(getGameLevel);
 		}, 1000);
+		console.log(`numberOfLives before: ${numberOfLives}`);
+		numberOfLives--;
+		console.log(`numberOfLives after: ${numberOfLives}`);
 		
 
 	}else if (userClickPattern[lastColor] !== gamePattern[lastColor] && numberOfLives === 0) { //comparing values in an array
@@ -200,11 +191,7 @@ function checkAns() {
 		return;
 	}else if (userClickPattern.length === gamePattern.length) {
 		console.log('Success');
-		console.log(`gamePattern: ${gamePattern}, userClickPattern: ${userClickPattern}`);
-		console.log(`gamePattern.length: ${gamePattern.length}, userClickPattern.length: ${userClickPattern.length}`);
-		// if (gameLevel < maxLevel) {
 		 	gameLevel++;
-		//   }
 		setTimeout(() => {
             generateColorCombination(getGameLevel);
 		}, 1000);
@@ -219,7 +206,6 @@ function restartGame(){
     $('.pixel-btn').click(function(){
 		location.reload();
 	})
-    
 }
 
 function playSound(buttonText){
